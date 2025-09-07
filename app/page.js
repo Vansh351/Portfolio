66efa809fae6b50fe8c8fad56408de3b23d63090
Vansh350 <Chauhanvansh350@gmail.com>
2025-09-07 20:54:34 +0530

    Eliminating prerendering error

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ import Skills from "./components/homepage/skills";
 export default function Home() {
   return (
     <div suppressHydrationWarning >
-      <HeroSection />
+      {/* <HeroSection /> */}
       {/* <AboutSection />
       <Experience />
       <Skills />
