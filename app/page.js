73a237aa34632689ad6266b794f6d393510a7976
Vansh350 <Chauhanvansh350@gmail.com>
2025-09-07 22:12:25 +0530

    comment contact section

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
+// import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 // import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
@@ -17,7 +17,7 @@ export default function Home() {
       {/* <Skills /> */}
       {/* <Projects /> */}
       <Education />
-      <ContactSection />
+      {/* <ContactSection /> */}
     </div>
   )
 };
