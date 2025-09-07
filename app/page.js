6241538375425463b755bd6774af6b9c5452dbd9
Vansh350 <Chauhanvansh350@gmail.com>
2025-09-07 21:58:33 +0530

    Adding experience

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ import AboutSection from "./components/homepage/about";
 // import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
+// import Projects from "./components/homepage/projects";
+// import Skills from "./components/homepage/skills";
 
 export default function Home() {
   return (
@@ -14,8 +14,8 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <Experience />
-      <Skills />
-      <Projects />
+      {/* <Skills />
+      <Projects /> */}
       {/* <Education />
       <ContactSection /> */}
     </div>
