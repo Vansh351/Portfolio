464afbbdf0d2a25d27098532c3364069616b9964
Vansh350 <Chauhanvansh350@gmail.com>
2025-09-07 21:22:51 +0530

    Cooment home components

--- app/page.js
+++ app/page.js
@@ -11,13 +11,13 @@ import Skills from "./components/homepage/skills";
 export default function Home() {
   return (
     <div suppressHydrationWarning >
-      <HeroSection />
+      {/* <HeroSection />
       <AboutSection />
       <Experience />
       <Skills />
       <Projects />
       <Education />
-      <ContactSection />
+      <ContactSection /> */}
     </div>
   )
 };
