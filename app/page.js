e6450cefd69f70ecb9caf2a8af47a0665282a37c
Vansh350 <Chauhanvansh350@gmail.com>
2025-09-07 23:00:32 +0530

    Add component section

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
-// import ContactSection from "./components/homepage/contact";
+import ContactSection from "./components/homepage/contact";
 
 export default function Home() {
   return (
@@ -17,7 +17,7 @@ export default function Home() {
       <Skills />
       <Projects />
       <Education />
-      {/* <ContactSection /> */}
+      <ContactSection />
     </div>
   )
 };
