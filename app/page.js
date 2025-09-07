bf866cb8e99c433ae9adaf3419102ba69bb5d362
Vansh350 <Chauhanvansh350@gmail.com>
2025-09-07 21:56:22 +0530

    Uncomment home sections

--- app/page.js
+++ app/page.js
@@ -3,20 +3,20 @@
 import AboutSection from "./components/homepage/about";
 // import ContactSection from "./components/homepage/contact";
 // import Education from "./components/homepage/education";
-// import Experience from "./components/homepage/experience";
+import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
-// import Projects from "./components/homepage/projects";
-// import Skills from "./components/homepage/skills";
+import Projects from "./components/homepage/projects";
+import Skills from "./components/homepage/skills";
 
 export default function Home() {
   return (
     <div suppressHydrationWarning >
       <HeroSection />
       <AboutSection />
-      {/* <Experience />
+      <Experience />
       <Skills />
       <Projects />
-      <Education />
+      {/* <Education />
       <ContactSection /> */}
     </div>
   )
