7d2e6768c507fdcc063a01ea8156adcb3f333a3b
Vansh350 <Chauhanvansh350@gmail.com>
2025-09-08 01:01:27 +0530

    Modify footer and hero section

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -13,12 +13,12 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href={personalData.linkedIn} className="text-[#16f2b3]">Vansh Chauhan</Link>
+            ©Portfolio of <Link target="_blank" href={personalData.linkedIn} className="text-[#16f2b3]">Vansh Chauhan</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio"
+              href="https://github.com/Vansh351/Portfolio/"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
