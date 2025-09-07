05e7c3612d22a88c0ffe8d6690060a11a761cbbe
Vansh350 <Chauhanvansh350@gmail.com>
2025-09-07 22:04:06 +0530

    comment project and experience

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 // import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
+// import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
 export default function Home() {
@@ -15,7 +15,7 @@ export default function Home() {
       <AboutSection />
       {/* <Experience /> */}
       <Skills />
-      <Projects />
+      {/* <Projects /> */}
       <Education />
       <ContactSection />
     </div>
